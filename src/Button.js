ebfd7132b228605f686c194c2670add57f12e0a4
ianglover28 <ianglover31.com>
2024-01-06 09:52:47 +0000

    initial commit

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Button () {
+    return (
+        <>
+            <button class="copy">
+  <span data-text-end="Copied!" data-text-initial="Copy to clipboard" class="tooltip"></span>
+  <span>
+   
+  </span>
+</button>
+        </>
+    )
+}
+
+export default Button ;
---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Main.css';
+
+
+
+function Main () {
+    return (
+        <>
+        <div id='section'>
+        <h1>There's still time to work wonders.</h1>
+         <p>Finish off your list with great last-minute gifts</p>
+          </div>
+    </>
+    );
+}
+
+
+export default Main;
